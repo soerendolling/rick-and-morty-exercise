1cd51564f5fa7352077a0da8a95e4a1b01b64956
Soeren Dolling <soeren.dolling@gmail.com>
2021-03-25 19:34:06 +0100

    Add basic html and CSS

---
+++ src/lib/character-card.js
@@ -0,0 +1,30 @@
+// const url = "https://rickandmortyapi.com/"
+
+// const getCharacter = fetch(url)
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error(response.status);
+//     }
+//   })
+//   .then((dataObject) => {
+//     dataObject.results.forEach((character) => {
+//       const section = document.createElement("section");
+//       section.classList.add("person");
+//       document.body.append(section);
+
+//       const h2 = document.createElement("h2");
+//       h2.textContent = `${person.name.title} ${person.name.first} ${person.name.last}`;
+//       section.append(h2);
+
+//       const img = document.createElement("img");
+//       img.src = `${person.picture.large}`;
+//       img.alt = `${person.name.first}`;
+//       section.append(img);
+//       console.log(person);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
